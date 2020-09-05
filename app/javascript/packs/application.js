02a0a94a00cc2e7ef6364f50595eb007a23b5cbc
cunhafernando <cunhafernandofj@gmail.com>
2020-09-04 22:00:29 -0300

    webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,5 @@ import ('js/site')
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //Images
-// Images
 require.context('../images', true)
-const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
