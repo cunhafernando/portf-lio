86ed7dd552c5a123aa7a53386fd913a3c75a4cbc
cunhafernando <cunhafernandofj@gmail.com>
2020-09-09 09:02:45 -0300

    alterando jquery

--- app/javascript/js/site.js
+++ app/javascript/js/site.js
@@ -1,4 +1,2 @@
 import 'bootstrap'
-import 'startbootstrap-resume/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'jquery.easing/jquery.easing.min.js'
 import 'startbootstrap-resume/dist/js/scripts.js'
