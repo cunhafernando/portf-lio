76327910bec9ee16648a494ccb3deba86f5d802f
cunhafernando <cunhafernandofj@gmail.com>
2020-09-09 01:37:06 -0300

    inserindo fontes e bootstrap

---
+++ app/javascript/js/site.js
@@ -0,0 +1,4 @@
+import 'bootstrap'
+import 'startbootstrap-resume/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'jquery.easing/jquery.easing.min.js'
+import 'startbootstrap-resume/dist/js/scripts.js'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,27 +1,3 @@
 const { environment } = require('@rails/webpacker')
-    // resolve-url-loader must be used before sass-loader
-environment.loaders.get('sass').use.splice(-1, 0, {
-    loader: 'resolve-url-loader'
-})
-
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-
-}))
-
-const config = environment.toWebpackConfig()
-
-environment.config.merge({
-    resolve: {
-        alias: {
-            jquery: 'jquery/src/jquery',
-        }
-    }
-})
 
 module.exports = environment
